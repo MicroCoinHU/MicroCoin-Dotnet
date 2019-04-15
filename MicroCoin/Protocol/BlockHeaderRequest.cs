1b5284957bca863f47d5ddb239928359c5a1a817
Németh Péter <sokkis@gmail.com>
2019-04-15 09:52:48 +0200

    Header request

---
+++ MicroCoin/Protocol/BlockHeaderRequest.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// This file is part of MicroCoin - The first hungarian cryptocurrency
+// Copyright (c) 2019 Peter Nemeth
+// BlockRequest.cs - Copyright (c) 2019 Németh Péter
+//-----------------------------------------------------------------------
+// MicroCoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MicroCoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU General Public License for more details.
+//-----------------------------------------------------------------------
+// You should have received a copy of the GNU General Public License
+// along with MicroCoin. If not, see <http://www.gnu.org/licenses/>.
+//-----------------------------------------------------------------------
+using MicroCoin.Common;
+using System.IO;
+using System.Text;
+
+namespace MicroCoin.Protocol
+{
+    public class BlockHeaderRequest : BlockRequest
+    {
+        public override NetOperationType NetOperation => NetOperationType.BlockHeader;
+
+        public BlockHeaderRequest()
+        {
+        }
+    }
+}
