91db9113819d4bbfc966f3e59774029107228b13
Németh Péter <sokkis@gmail.com>
2019-04-15 16:40:51 +0200

    New transaction request handler

---
+++ MicroCoin/Handlers/NewTransactionHandler.cs
@@ -0,0 +1,34 @@
+﻿using MicroCoin.Net;
+using MicroCoin.Protocol;
+using MicroCoin.Transactions;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroCoin.Handlers
+{
+    public class NewTransaction : PubSubEvent<ITransaction> { }
+    public class NewTransactionHandler : IHandler<NewTransactionRequest>
+    {
+        private readonly IEventAggregator eventAggregator;
+        private readonly object handlerLock = new object();
+
+        public NewTransactionHandler(IEventAggregator eventAggregator)
+        {
+            this.eventAggregator = eventAggregator;
+        }
+
+        public void Handle(NetworkPacket packet)
+        {
+            lock (handlerLock)
+            {
+                var request = packet.Payload<NewTransactionRequest>();
+                foreach (var transaction in request.Transactions)
+                {
+                    eventAggregator.GetEvent<NewTransaction>().Publish(transaction);
+                }
+            }
+        }
+    }
+}
