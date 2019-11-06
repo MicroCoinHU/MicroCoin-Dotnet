363b38c586a2104c35f7c3672b0ae89168f53b0c
Németh Péter <sokkis@gmail.com>
2019-11-06 14:04:34 +0100

    KeyStore & key management

---
+++ MicroCoin/KeyStore/IKeyStore.cs
@@ -0,0 +1,15 @@
+﻿using MicroCoin.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroCoin.KeyStore
+{
+    public interface IKeyStore
+    {
+        void Add(KeyPair keyPair);
+        IEnumerable<KeyPair> All();
+        int Count();
+        void Remove(KeyPair keyPair);
+    }
+}
