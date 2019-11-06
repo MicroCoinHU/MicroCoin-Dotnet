c4c26bdb290bf887b75cdc2abd45d11a7f768675
Németh Péter <sokkis@gmail.com>
2019-11-06 14:03:29 +0100

    Modularization support

---
+++ MicroCoin/Modularization/IModule.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroCoin.Modularization
+{
+    public interface IModule
+    {
+        public string Name { get; }
+        void RegisterModule(ServiceCollection serviceCollection);
+        void InitModule();
+    }
+}
---
+++ MicroCoin/Modularization/ModuleManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+
+namespace MicroCoin.Modularization
+{
+    public class ModuleManager
+    {
+        public void LoadModules(ServiceCollection serviceCollection)
+        {
+            Type imoduleType = typeof(IModule);
+            string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            foreach (var file in Directory.GetFiles(assemblyPath, "MicroCoin*.dll"))
+            {
+                AssemblyLoadContext.Default.Resolving += ResolveAssembly;
+                var types = AssemblyLoadContext.Default.LoadFromAssemblyPath(file).GetExportedTypes().Where(p => imoduleType.IsAssignableFrom(p) && p.IsClass);
+                if (types.Count() > 0)
+                {
+                    foreach(var t in types)
+                    {
+                        IModule module = (IModule) Activator.CreateInstance(t);
+                        module.RegisterModule(serviceCollection);
+                        LogManager.GetCurrentClassLogger().Debug("{0} loaded", module.Name);
+                    }
+                }
+            }
+        }
+
+        private Assembly ResolveAssembly(AssemblyLoadContext arg1, AssemblyName arg2)
+        {
+            return Assembly.LoadFrom(arg2.Name + ".dll");
+        }
+    }
+}
